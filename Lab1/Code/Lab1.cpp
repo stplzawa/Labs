564bd2e70eb5ddfa7bd9b3bcb00a489102a66cf7
backtrack <55458936+stplzawa@users.noreply.github.com>
2020-12-23 16:30:19 +0300

    Add files via upload

---
+++ Lab1/Code/Lab1.cpp
@@ -0,0 +1,17 @@
+﻿//Начальный код
+/*#include <iostream>
+int main() {
+    int i;
+    i = 5;
+    std::cout << i;
+    return 0;
+}*/ 
+
+//Изменение кода 
+/*#include <iostream>
+int main() {
+    double i;
+    i = 5;
+    std::cout << i;
+    return 0;
+}*/
